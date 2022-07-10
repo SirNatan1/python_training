36e798c75010fb841d0158a80d9212696f5e9a78
SirNatan1 <natan.irmatov@coralogix.com>
2022-07-10 11:58:06 +0300

    init

--- Day2/variabless.py
+++ Day2/variabless.py
@@ -1,18 +1,18 @@
 
 # Variables in Python
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+first_name = 'Natan'
+last_name = 'Ir'
+country = 'IL'
+city = 'Rishon'
+age = 69
+is_married = False
+skills = ['Banana', 'Python']
 person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
+    'firstname':'Natan', 
+    'lastname':'Ir', 
+    'country':'IL',
+    'city':'Rishon'
     }
 
 # Printing the values stored in the variables
@@ -30,7 +30,7 @@ print('Person information: ', person_info)
 
 # Declaring multiple variables in one line
 
-first_name, last_name, country, age, is_married = 'Natan', 'Ir', 'Israhel', 69, False
+first_name, last_name, country, age, is_married, is_light_on = 'Natan', 'Ir', 'Israhel', 69, False, False
 
 print(first_name, last_name, country, age, is_married)
 print('First name:', first_name)
@@ -38,3 +38,38 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+print('is light on?', is_light_on)
+
+print(type(first_name))
+print(type(person_info))
+print(type(is_married))
+print(len(first_name))    #print length of var
+
+num_one, num_two = 5, 4     #set 2 vars
+num_three = num_one + num_two       #sum of the 2 vars
+print(num_three)        #print the var of the sum
+
+num_four = num_one - num_two
+print(num_four)
+
+num_five = num_one * num_two
+print(num_five)
+
+num_six = num_one / num_two
+print(num_six)
+
+num_seven = num_one % num_two
+print(num_seven)
+
+num_eight = num_one ** num_two
+print(num_eight)
+
+# pi = 3.14
+# radius = float(input("radius:"))
+# A = pi * radius ** 2
+# print("the area is:", A)
+
+name = input('name: ')
+last_name = input('last name:')
+age = input('age: ')
+print(name, last_name, age)
