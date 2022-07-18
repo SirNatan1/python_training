9a9a17f2271896258748b79352784f68845c577e
SirNatan1 <natan.irmatov@coralogix.com>
2022-07-18 17:02:41 +0300

    init

---
+++ Day3/variables.py
@@ -0,0 +1,36 @@
+# age = 24
+# hight = 1.80
+# complex = 5 + 2j
+
+# h = float(input('enter triangle hight: '))
+# x = float(input('enter triangle base: '))
+# a = x * h / 2
+# print('the area of the triangle is: ', a)
+
+# a = float(input('enter length of side a: '))
+# b = float(input('enter length of side b: '))
+# c = float(input('enter length of side c: '))
+# x = a + b + c
+# print('the perimiter is: ', x)
+
+# length = float(input('enter length: '))
+# width = float(input('enter width: '))
+# area = length * width
+# perimiter = (length + width) * 2
+# print('the area is: ', area)
+# print('the perimiter is: ', perimiter)
+
+# r = float(input('enter radius: '))
+# pi = 3.14
+# area = pi * r ** 2
+# c = 2 * pi * r
+# print('area is: ', area)
+# print('frame is: ', c)
+
+#Calculate the slope, x-intercept and y-intercept of y = 2 * x - 2
+x1 = 0
+y1 = 2 * x1 -2
+print(y1)
+y2 = 0
+x2 = y2 = 2 * x2 -2 
+print(x2)
