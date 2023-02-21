dc730b0aa40b1317dd17a4fc7bd498ba2125769c
SirNatan1 <natan757@gmail.com>
2023-02-21 22:13:47 +0200

    Added: improvements

--- Day9/contactbook.py
+++ Day9/contactbook.py
@@ -62,3 +62,26 @@ def contactbook():
         print('This name dosent exist in the contactbook')
 
 contactbook()
+
+#add conditional for user input of a new entry
+#maybe it'll cause restructure of the whole dictionary by deleting the key of the name
+#then change the condition of the if to a loop which will do the same but check inside the dict for a value
+#it'll be like this:
+
+# for new_name in names:
+#         if new_name['Name'] == name:
+#             print(new_name)
+#             break
+#     else:
+#         print('This name dosent exist in the contactbook')
+
+#utilize something as such:
+
+# new_key = input('Enter the new name: ')
+# new_value1 = input('Enter the first value: ')
+# new_value2 = input('Enter the second value: ')
+# new_value3 = input('Enter the third value: ')
+
+# my_dict[new_key] = (new_value1, new_value2, new_value3)
+
+# print(my_dict)
