3d1795ae1b291ab828d8284eba8b60bb5d1cc4cd
SirNatan1 <natan757@gmail.com>
2023-02-22 18:24:02 +0200

    Added: even more Ideas

--- Day9/contactbook.py
+++ Day9/contactbook.py
@@ -64,6 +64,7 @@ def contactbook():
 contactbook()
 
 # add loop to make it as CLI (press e for edit or c for create)
+# think of a way to utillize functions, to make it cleaner, try to devide it into 2 maybe.. the dict and the conditionals.
 #add conditional for user input of a new entry
 #maybe it'll cause restructure of the whole dictionary by deleting the key of the name
 #then change the condition of the if to a loop which will do the same but check inside the dict for a value
